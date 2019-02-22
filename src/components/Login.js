ee12c843e59cd8878729c03c964acdb53fb83b84
HeshamHamadaALG <heshamhamada222@gmail.com>
2019-02-23 00:50:31 +0200

    motion to form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import QueueAnim from 'rc-queue-anim';
 
 
 class Login extends Component {
@@ -31,22 +32,32 @@ class Login extends Component {
         return (
             <div className="FormCenter">
           <form onClick={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+          <QueueAnim
+            component="div"
+            animConfig={[
+              { opacity: [1, 0], translateX: [0, 30] },
+              { height: 0 },
+            ]}
+            ease={['easeOutQuart', 'easeInOutQuart']}
+            duration={[550, 450]}
+            interval={150}
+          >
 
-            <div className="Field">
+            <div key="a" className="Field">
               <label className="Label" htmlFor="email">E-mail</label>
               <input type="email" id="email" className="Input" placeholder="Write Your E-mail" name="email" value={this.state.email} onChange={this.handleChange}></input>
             </div>
 
-            <div className="Field">
+            <div key="b" className="Field">
               <label className="Label" htmlFor="password">Password</label>
               <input type="password" id="password" className="Input" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange}></input>
             </div>
 
-            <div className="Field">
+            <div key="c" className="Field">
               <button className="fieldButton">Login</button>
               <Link to="/Signup" className="link" >Don't Have Account ?</Link>
             </div>
-          
+          </QueueAnim>
           </form>
 
         </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import QueueAnim from 'rc-queue-anim';
 
 
 class Signup extends Component {
@@ -8,38 +9,48 @@ class Signup extends Component {
         return (
             <div className="FormCenter">
           <form className="FormFields" onSubmit={this.handleSubmit}>
-            <div className="Field">
+          <QueueAnim
+            component="div"
+            animConfig={[
+              { opacity: [1, 0], translateX: [30, 0] },
+              { height: 0 },
+            ]}
+            ease={['easeOutQuart', 'easeInOutQuart']}
+            duration={[550, 450]}
+            interval={150}
+          >
+            <div key="a" className="Field">
               <label className="Label" htmlFor="firstname">First Name</label>
               <input type="text" id="firstname" className="Input" placeholder="Write Your First Name" name="firstname" ></input>
             </div>
 
-            <div className="Field">
+            <div key="b" className="Field">
               <label className="Label" htmlFor="lastname">Last Name</label>
               <input type="text" id="lastname" className="Input" placeholder="Write Your Last Name" name="lastname" ></input>
             </div>
 
-            <div className="Field">
+            <div key="c" className="Field">
               <label className="Label" htmlFor="email">E-mail</label>
               <input type="email" id="email" className="Input" placeholder="Write Your E-mail" name="email" ></input>
             </div>
 
-            <div className="Field">
+            <div key="d" className="Field">
               <label className="Label" htmlFor="password">Password</label>
               <input type="password" id="password" className="Input" placeholder="Password" name="password" ></input>
             </div>
 
-            <div className="Field">
+            <div key="e" className="Field">
               <label className="Label" htmlFor="password">Confirm Password</label>
               <input type="password" id="password" className="Input" placeholder="Retype Password" name="password" ></input>
             </div>
 
             {/* Handle Image Upload */}
 
-            <div className="Field">
+            <div key="f" className="Field">
               <button className="fieldButton">SignUp</button>
               <Link to="/" className="link" >I'm already member</Link>
             </div>
-          
+            </QueueAnim>
           </form>
 
         </div>
