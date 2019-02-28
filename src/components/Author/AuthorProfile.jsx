f369f3c0b2f8933b02f4fbb75df95a45f468a525
Mohamed Gomaa <promgom3a@gmail.com>
2019-03-01 01:46:46 +0200

    handle display authors images.

--- src/components/Author/AuthorProfile.jsx
+++ src/components/Author/AuthorProfile.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../Shared/navbar';
 import AuthorBooks from './authorBooks';
-import AuthorCard from './AuthorCard' ; 
+import AuthorCard from './AuthorCard';
 
 
 class AuthorProfile extends Component {
@@ -9,29 +9,37 @@ class AuthorProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           books : [] ,
-            author : []
+            books: [],
+            author: null
         }
     }
-   
+
+    componentDidMount() {
+        fetch(`https://gomaanodejsapp.herokuapp.com/author/${this.props.id}`)
+            .then(response => response.json())
+            .then(result => {
+                this.setState({ author: result.author });
+                this.setState({ books: result.author_books })
+            }
+
+            );
+    }
 
     render() {
-        
-        fetch(`https://gomaanodejsapp.herokuapp.com/author/${this.props.id}`)
-        .then(response => response.json())
-        .then(result =>  {this.setState({ author: result.author }) ; 
-                         this.setState({ books: result.author_books })
-                         } 
-             
-            );
+
+        if (this.state.author == null){
+            console.log("Gomaa")
+            return null;
             
+        }
+
         return (
             <div>
                 < Navbar />
                 <div class="container Author-page">
                     <div class="row">
-                    {console.log(`Data sent from profile: ${this.state.author.first_name}`)}
-                    <AuthorCard authorInfo = {this.state.author} className="col-md-4"   />
+
+                        <AuthorCard authorInfo={this.state.author} className="col-md-4" />
                     </div>
                 </div>
 
@@ -40,7 +48,7 @@ class AuthorProfile extends Component {
                     <div className="row">
 
                         <h3 style={{ margin: "20px" }}>Author's Books</h3>
-                        <AuthorBooks books ={this.state.books} />
+                        <AuthorBooks books={this.state.books} />
                     </div>
                 </div>
 
