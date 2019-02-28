851eb653e45a3f715f40957d221571ad96016c80
Mk1499 <m.khaled1499@gmail.com>
2019-03-01 00:17:45 +0200

    add user id to book card navigation

--- src/components/CategoryBooks/BookCard.js
+++ src/components/CategoryBooks/BookCard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import {Link} from '@reach/router';
 
+
+
 export default class BookCard extends Component {
 
+    
     arrayBufferToBase64(buffer) {
         var binary = '';
         var bytes = [].slice.call(new Uint8Array(buffer));
@@ -10,16 +13,17 @@ export default class BookCard extends Component {
         return binary;
     };
 
+    
 
     render() {
-        
+        let userId = localStorage.getItem("userId") ; 
         const image = this.arrayBufferToBase64(this.props.book_img.data.data);
 
         return (
             <div className="card book-card-container" style={{ width: "18rem" }}>
                 <img src={image} className="card-img-top" alt="Image not Found" />
                 <div className="card-body">
-                    <Link to={`/book/${this.props.book_id}`} >{this.props.book_name}</Link><br />
+                    <Link to={`/book/${this.props.book_id}/${userId}`} >{this.props.book_name}</Link><br />
                     <Link to={`/author/${this.props.author_id}`} >By {this.props.author_name}</Link>
                 </div>
             </div>
